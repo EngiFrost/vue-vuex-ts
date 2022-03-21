9551613152ca9af61f3673017cad8cec1a99eb44
Daniel Frost <flint.bets@yandex.ru>
2022-03-21 21:29:55 +0300

    The beginning!

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <main>
+    <header id="main">
+      <Navbar />
+    </header>
+
+    <RouterView />
+  </main>
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  border: 2px solid black;
+  font-weight: normal;
+}
+</style>
---
+++ src/views/TodosView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main id="main"></main>
+</template>
+
+<style>
+#main {
+  height: 80vh;
+  width: 100%;
+}
+</style>
