3655fe4f217a98316d5e43c7751c2b5b100a9c85
Daniel Frost <flint.bets@yandex.ru>
2022-03-22 22:28:52 +0300

    Moved navbar to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <main>
     <header id="main">
-      <nav id="navbar">
-        <div id="btn-wrapper">
-          <RouterLink to="/">Todos</RouterLink>
-        </div>
-      </nav>
+      <Navbar />
     </header>
 
     <RouterView />
@@ -23,30 +20,5 @@ import { RouterLink, RouterView } from "vue-router";
   padding: 2rem;
   border: 2px solid black;
   font-weight: normal;
-}
-
-#navbar {
-  width: 100%;
-  height: 60px;
-  border: 4px solid rgb(8, 80, 104);
-  display: flex;
-  flex-direction: row-reverse;
-  align-items: center;
-}
-
-#btn-wrapper {
-  padding: 10px;
-  border: 3px solid rgb(0, 62, 133);
-  height: fit-content;
-  width: fit-content;
-  margin-right: 10px;
-}
-
-#btn-wrapper > a {
-  text-transform: uppercase;
-  font-size: 14px;
-  font-weight: 700;
-  text-decoration: none;
-  color: black;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+
+interface Route {
+  link: string,
+  title: string,
+}
+
+const routes: Route[] = [
+  { link: '/', title: "Todos" }
+];
+</script>
+
+<template>
+  <nav id="navbar">
+    <div id="btn-wrapper" v-for="route in routes">
+      <RouterLink to="{{route.link}}">{{ route.title }}</RouterLink>A
+    </div>
+  </nav>
+</template>
+
+<style>
+#navbar {
+  width: 100%;
+  height: 60px;
+  border: 4px solid rgb(8, 80, 104);
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+}
+#btn-wrapper {
+  padding: 10px;
+  border: 3px solid rgb(0, 62, 133);
+  height: fit-content;
+  width: fit-content;
+  margin-right: 10px;
+}
+#btn-wrapper > a {
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: 700;
+  text-decoration: none;
+  color: black;
+}
+</style>
