11763f8b4b6c5923d78e3eb7d216175999485f75
Daniel Frost <flint.bets@yandex.ru>
2022-03-23 19:59:58 +0300

    Created todo form component

---
+++ src/components/AddTodoForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <form class="form">
+      <input type="text" class="input" />
+      <div class="btn-wrapper">
+        <button class="button">Add</button>
+        <button class="button">Clear checked</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: 50px;
+}
+.form {
+  border: 1px solid black;
+}
+.input {
+  width: 100%;
+}
+
+.btn-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+.button {
+  border: 3px solid rgb(134, 134, 134);
+}
+</style>
