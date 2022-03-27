f2fe0a21bb603c59ae4189c3e397d901bf11d779
Daniel Frost <flint.bets@yandex.ru>
2022-03-27 19:30:39 +0300

    Sass integration

--- src/components/AddTodoForm.vue
+++ src/components/AddTodoForm.vue
@@ -11,28 +11,32 @@
 
 <script setup lang="ts"></script>
 
-<style scoped>
+<style lang="scss" scoped>
 .form-wrapper {
   display: flex;
   flex-direction: column;
   width: 300px;
-  height: 70px;
   border: 3px solid red;
 }
 .input {
   display: flex;
   box-sizing: border-box;
   width: 100%;
-  height: 20px;
+  height: 30px;
 }
 
 .btn-wrapper {
   display: flex;
   justify-content: space-around;
-  height: 30px;
 }
 .button {
-  border: 3px solid rgb(134, 134, 134);
-  flex: 1;
+  border: 2px solid rgb(134, 134, 134);
+  border-radius: 5px;
+  height: 30px;
+  width: 120px;
+  margin: 5px;
+  &:hover {
+    background-color: lightgray;
+  }
 }
 </style>
